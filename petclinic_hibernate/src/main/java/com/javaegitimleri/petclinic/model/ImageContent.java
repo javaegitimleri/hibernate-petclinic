b2f08b1395505f09fcd52220fa0e309d86a0e2df
ksevindik <ksevindik@harezmi.com.tr>
2019-01-09 15:02:17 +0200

    entity_assocs_unidirectional_onetoone_with_pk

--- petclinic_hibernate/src/main/java/com/javaegitimleri/petclinic/model/ImageContent.java
+++ petclinic_hibernate/src/main/java/com/javaegitimleri/petclinic/model/ImageContent.java
@@ -1,12 +1,23 @@
 package com.javaegitimleri.petclinic.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="t_image_content")
-public class ImageContent extends BaseEntity {
+public class ImageContent {
+	
+	@Id
+	private Long id;
+	
+	@OneToOne
+	@MapsId
+	private Image image;
+	
 	@Lob
 	private byte[] content;
 
@@ -17,4 +28,22 @@ public class ImageContent extends BaseEntity {
 	public void setContent(byte[] content) {
 		this.content = content;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+	
+	
 }
