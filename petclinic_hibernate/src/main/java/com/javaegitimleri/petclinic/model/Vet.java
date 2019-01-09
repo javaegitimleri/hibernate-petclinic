ff684aceead9df54e8420d7ae316a7b433009866
ksevindik <ksevindik@harezmi.com.tr>
2019-01-09 14:30:34 +0200

    entity_assocs_unidirectional_manytomany

--- petclinic_hibernate/src/main/java/com/javaegitimleri/petclinic/model/Vet.java
+++ petclinic_hibernate/src/main/java/com/javaegitimleri/petclinic/model/Vet.java
@@ -1,14 +1,24 @@
 package com.javaegitimleri.petclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="t_vet")
+@Table(name = "t_vet")
 public class Vet extends Person {
-	@Column(name="works_full_time")
+	@Column(name = "works_full_time")
 	private Boolean worksFullTime;
+
+	@ManyToMany
+	@JoinTable(name = "t_vet_specialty", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
+	private Set<Specialty> specialties = new HashSet<>();
 
 	public Boolean getWorksFullTime() {
 		return worksFullTime;
