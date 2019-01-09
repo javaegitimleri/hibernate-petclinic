6facce8848a965ea06b90474bd20a55a908b266a
ksevindik <ksevindik@harezmi.com.tr>
2019-01-09 12:57:18 +0200

    entity_assocs_onetomany_with_jointable

--- petclinic_hibernate/src/main/java/com/javaegitimleri/petclinic/model/Clinic.java
+++ petclinic_hibernate/src/main/java/com/javaegitimleri/petclinic/model/Clinic.java
@@ -1,13 +1,28 @@
 package com.javaegitimleri.petclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="t_clinic")
+@Table(name = "t_clinic")
 public class Clinic extends BaseEntity {
 	private String name;
 
+	@OneToMany
+	@JoinTable(name = "t_clinic_owner", joinColumns = @JoinColumn(name = "clinic_id"), inverseJoinColumns = @JoinColumn(name = "owner_id"))
+	private Set<Owner> owners = new HashSet<>();
+
+	@OneToMany
+	@JoinTable(name = "t_clinic_vet", joinColumns = @JoinColumn(name = "clinic_id"), inverseJoinColumns = @JoinColumn(name = "vet_id"))
+	private Set<Vet> vets = new HashSet<>();
+
+	
 	public String getName() {
 		return name;
 	}
@@ -15,6 +30,5 @@ public class Clinic extends BaseEntity {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+
 }
