a9fb10db2123d66361f932f5b739babd64ac47ac
ksevindik <ksevindik@harezmi.com.tr>
2019-01-09 14:35:50 +0200

    entity_assocs_bidirectional_manytomany

--- petclinic_hibernate/src/main/java/com/javaegitimleri/petclinic/model/Specialty.java
+++ petclinic_hibernate/src/main/java/com/javaegitimleri/petclinic/model/Specialty.java
@@ -1,12 +1,19 @@
 package com.javaegitimleri.petclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="t_specialty")
 public class Specialty extends BaseEntity {
 	private String name;
+	
+	@ManyToMany(mappedBy="specialties")
+	private Set<Vet> vets = new HashSet<>();
 
 	public String getName() {
 		return name;
