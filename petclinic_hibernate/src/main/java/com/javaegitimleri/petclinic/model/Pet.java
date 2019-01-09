18d798e932d393ad95e5bb4dd0c8c404074be90a
ksevindik <ksevindik@harezmi.com.tr>
2019-01-09 13:48:11 +0200

    entity_assocs_unidirectional_onetomany_with_bag

--- petclinic_hibernate/src/main/java/com/javaegitimleri/petclinic/model/Pet.java
+++ petclinic_hibernate/src/main/java/com/javaegitimleri/petclinic/model/Pet.java
@@ -1,15 +1,15 @@
 package com.javaegitimleri.petclinic.model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -35,6 +35,10 @@ public class Pet extends BaseEntity {
 	@ManyToOne
 	@JoinColumn(name="owner_id")
 	private Owner owner;
+	
+	@OneToMany
+	@JoinColumn(name="pet_id")
+	private List<Visit> visits = new ArrayList<>();
 	
 	public Pet() {
 		
